fd47afd05a99e171ac6b37291010ce1f061c1d02
VincentG42 <goyetvincent@gmail.com>
2023-11-15 12:32:57 +0100

    responsive jusqu a 700px

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 async function afficherFilms() {
-    const reponse = await fetch("http://127.0.0.1:5500/Projet-allo-cine/data/moovies.json");
+    const reponse = await fetch("http://127.0.0.1:5500/data/moovies.json");
     const films = await reponse.json();
     console.log(films)
     films.forEach(film => {
@@ -8,19 +8,20 @@ async function afficherFilms() {
             <img class="posterfilm" src="${film.Poster}">
             <div class="flex2">
                 <h2 class="titrefilm">${film.Title}</h2>
-                
-                <p class="datedesortie"> ${film.released}/${film.Runtime}/ ${film.Genre}</p>
-                <p class="distribution"><span>Avec</span> ${film.Actors}</p>
-                <p class="realisateur"> <span>Un film de</span> ${film.Director}, <span>Ecrit par</span> ${film.Writer}</p>
-                <p class="synopsys"> ${film.Plot}</p>
-                <div class="basdecarte">
-                    <p class="notes"> <span>mdbRating:</span> ${film.mdbRating}</p> <p class="notes"><span> Metascore: ${film.Metascore}</span> </p>
-                    <div>${film.Metascore === undefined ? vraie : ' '}
-                        <img  class="imagesfilm" src="${film.Images[0]}">
-                        <img  class="imagesfilm" src="${film.Images[1]}">
-                        <img  class="imagesfilm" src="${film.Images[2]}">
-                    </div>
-                </div>    
+                <div class="textcarte">
+                    <p class="datedesortie"> ${film.Released ? film.Released : '-'}/${film.Runtime ? film.Runtime : '-'}/ ${film.Genre}</p>
+                    <p class="distribution"><span>Avec</span> ${film.Actors}</p>
+                    <p class="realisateur"> <span>Un film de</span> ${film.Director}, <span>Ecrit par</span> ${film.Writer}</p>
+                    <p class="synopsys"> ${film.Plot}</p>
+                    <div class="basdecarte">
+                        <p class="notes"> <span>imdbRating:</span> ${film.imdbRating}</p> <p class="notes"><span> Metascore:</span> ${film.Metascore} </p>
+                        
+                            <img  class="imagesfilm" src="${film.Images[0]}">
+                            <img  class="imagesfilm" src="${film.Images[1]}">
+                            <img  class="imagesfilm" src="${film.Images[2]}">       
+                        
+                    </div>    
+                </div>
             </div>
        </div>
        `
